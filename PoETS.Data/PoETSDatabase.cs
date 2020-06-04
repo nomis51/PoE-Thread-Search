0e86d45abab76dc7004eee967265c0c98317bf45
Simon Thomas <nomis.guit.51@gmail.com>
2020-06-04 18:58:30 -0400

    Concurency handling for sqlite database interactions

--- PoETS.Data/PoETSDatabase.cs
+++ PoETS.Data/PoETSDatabase.cs
@@ -26,31 +26,47 @@ namespace PoETS.Data {
         }
 
         public async Task<List<T>> Get<T>() where T : Model {
-            return await _context.Set<T>().ToListAsync();
+            _context.Lock.WaitOne();
+            var entities = await _context.Set<T>().ToListAsync();
+            _context.Lock.ReleaseMutex();
+
+            return entities;
         }
 
         public async Task<T> Get<T>(int id) where T : Model {
-            return (await Get<T>()).Find(t => t.Id == id);
+            _context.Lock.WaitOne();
+            var entity = (await Get<T>()).Find(t => t.Id == id);
+            _context.Lock.ReleaseMutex();
+
+            return entity;
         }
 
         public async Task<T> Insert<T>(T entity) where T : Model {
+            _context.Lock.WaitOne();
             await _context.Set<T>().AddAsync(entity);
             await _context.SaveChangesAsync();
+            _context.Lock.ReleaseMutex();
 
             return entity;
         }
 
         public async Task<T> Update<T>(T entity) where T : Model {
+            _context.Lock.WaitOne();
             _context.Set<T>().Update(entity);
             await _context.SaveChangesAsync();
+            _context.Lock.ReleaseMutex();
 
             return entity;
         }
 
         public async Task<T> Delete<T>(int id) where T : Model {
             var entity = await Get<T>(id);
+
+            _context.Lock.WaitOne();
             _context.Remove(entity);
             await _context.SaveChangesAsync();
+            _context.Lock.ReleaseMutex();
+
             return entity;
         }
 
