5daabd20d2e7317fbc4323c0ae372985f0a43700
Simon Thomas <nomis.guit.51@gmail.com>
2020-06-04 18:58:49 -0400

    Automatically handling primary key ids

--- PoETS.Data/Database/PoETSDbContext.cs
+++ PoETS.Data/Database/PoETSDbContext.cs
@@ -19,44 +19,9 @@ namespace PoETS.Data.Database {
             options.UseSqlite("Data Source=.\\data.db");
         }
 
-        public int NextId(Type type) {
-            switch (type) {
-                case typeof(Player):
-                    if (PlayersNextId == -1) {
-                        PlayersNextId = GetNextId(Players);
-                    }
-                    return PlayersNextId++;
-                case "Posts":
-                    if (PostsNextId == -1) {
-                        PostsNextId = GetNextId(Posts);
-                    }
-                    return PostsNextId++;
-                case "ForumThreads":
-                    if (ForumThreadsNextId == -1) {
-                        ForumThreadsNextId = GetNextId(ForumThreads);
-                    }
-                    return ForumThreadsNextId++;
-                case "Pages":
-                    if (PageNextId == -1) {
-                        PageNextId = GetNextId(Pages);
-                    }
-                    return PageNextId++;
-                default:
-                    return 0;
-            }
-        }
-
-        private int GetNextId<T>(DbSet<T> set) where T : Model {
-            return set.Count() > 0 ? set.Max(p => p.Id) + 1 : 1;
-        }
-
-        private int PlayersNextId = -1;
         public DbSet<Player> Players { get; set; }
-        private int PostsNextId = -1;
         public DbSet<Post> Posts { get; set; }
-        private int ForumThreadsNextId = -1;
         public DbSet<ForumThread> ForumThreads { get; set; }
-        private int PageNextId = -1;
         public DbSet<Page> Pages { get; set; }
 
     }
