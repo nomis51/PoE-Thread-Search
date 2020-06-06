71f8c7fe7e03dd87385cf8e1d8427eecdd3a8aa3
nomis51 <nomis.guit.51@gmail.com>
2020-06-06 19:56:28 -0400

    Update forumThread model

--- PoETS.Data/Models/ForumThread.cs
+++ PoETS.Data/Models/ForumThread.cs
@@ -6,11 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PoETS.Data.Models {
-    [Table("ForumThread")]
     public class ForumThread : Model {
         public string Name { get; set; }
         public Player Author { get; set; }
+        public int ThreadId { get; set; }
         public int NbPage { get; set; } = 1;
-        public List<Page> Pages { get; } = new List<Page>();
     }
 }
